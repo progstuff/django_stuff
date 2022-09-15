d9b80a23f557dce82dea038f0abfa0691e75f7de
Андрей Морозов <yerdnavozorom@gmail.com>
2022-09-15 16:43:23 +0300

    добавлена страница с объявлениями

--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++ 04_DatabasesAndModels/board/advertisements_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('advertisements', views.AdvertisementsList.as_view(), name='main_page'),
 ]
--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class AdvertisementsList(TemplateView):
+    template_name = 'advertisements_app/advertisements_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['address'] = "адрес"
+        context['phone'] = "8-xxx-xxx-xx-xx"
+        context['email'] = "mail@mail.ru"
+        return context
