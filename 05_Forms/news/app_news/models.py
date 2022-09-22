136c7e416641e2d02e54e5aba3bd6cda0e5371cd
Андрей Морозов <yerdnavozorom@gmail.com>
2022-09-22 14:12:58 +0300

    добавил модели

--- 05_Forms/news/app_news/models.py
+++ 05_Forms/news/app_news/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 
+
+class News(models.Model):
+
+    title = models.CharField(max_length=1000, default='', verbose_name='заголовок')
+    description = models.CharField(max_length=10000, verbose_name='Описание')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    is_active = models.BooleanField(default=True, verbose_name='Активна')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100, default='', verbose_name='Пользователь')
+    description = models.CharField(max_length=10000, verbose_name='Комментарий')
+    news = models.ForeignKey('News', default=None, null=True, on_delete=models.CASCADE,
+                             related_name="news", verbose_name='Новость')
+
+    def __str__(self):
+        return self.user_name
+
