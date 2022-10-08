1b4c98611dabf0bf111a0610f43f830b675732f4
Морозов Андрей <yerdnavozorom@gmail.com>
2022-10-08 23:20:23 +0300

    добавлена регистрация пользователя

--- 09_Files/djfiles/files_test/views.py
+++ 09_Files/djfiles/files_test/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
+from django.views import View
+from .forms import UserRegisterForm
 
 
 class AllPosts(TemplateView):
@@ -20,5 +23,19 @@ class PostCreate(TemplateView):
     template_name = 'files_test/post_create.html'
 
 
-class RegisterPage(TemplateView):
-    template_name = 'files_test/register_page.html'
+class RegisterPage(View):
+
+    def get(self, request, *args, **kwargs):
+        register_form = UserRegisterForm()
+        return render(request, 'files_test/register_page.html', context={'form': register_form})
+
+    def post(self, request):
+        register_form = UserRegisterForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect('all-posts')
+        else:
+            errors = register_form.errors
+            register_form = UserRegisterForm()
+
+            return render(request, 'files_test/register_page.html', context={'form': register_form, 'errors': errors.__str__()})
