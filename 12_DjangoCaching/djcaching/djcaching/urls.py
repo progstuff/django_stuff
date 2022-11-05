9bf38b2c9e64e9747f76ed8a99561396784836ce
Andrey Morozov <yerdnavozorom@gmail.com>
2022-11-05 14:23:32 +0300

    установлены зависимости для документирования

--- 12_DjangoCaching/djcaching/djcaching/urls.py
+++ 12_DjangoCaching/djcaching/djcaching/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/docs', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('i18n', include('django.conf.urls.i18n')),
     path('', include('shop.urls')),
