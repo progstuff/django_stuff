db0aa2bbfa31b1d2df5303a6aa37ea788c010865
Андрей Морозов <yerdnavozorom@gmail.com>
2022-11-10 15:49:14 +0300

    добавлен проект и приложение

--- 15_DatabasesAdvanced/marketplace/manage.py
+++ 15_DatabasesAdvanced/marketplace/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djmarketplace.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
