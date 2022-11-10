aa283049dc8fa4298455109c3c2c7e2179b3ac72
Андрей Морозов <yerdnavozorom@gmail.com>
2022-11-10 16:36:01 +0300

    добавлены шаблоны

--- 15_DatabasesAdvanced/marketplace/marketplace_cite/views.py
+++ 15_DatabasesAdvanced/marketplace/marketplace_cite/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+
+class BalancePage(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/add_balance_page.html', context={})
+
+class AuthenticationView(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/authentication_page.html', context={})
+
+class ProductsListView(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/personal_cabinet_page.html', context={})
+
+class ProductsListView(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/popular_products_page.html', context={})
+
+class ProductsListView(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/products_list_page.html', context={})
+
+class ProductsListView(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/purchase_page.html', context={})
+
+class RegistrationView(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/registration_page.html', context={})
+
+class ShoppingCartView(View):
+
+    def get(self, request):
+        return render(request, 'marketplace_cite/shopping_cart_page.html', context={})
