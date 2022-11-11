55c19ebd5b03720955e7589645c95853a9550cf5
Андрей Морозов <yerdnavozorom@gmail.com>
2022-11-11 13:37:00 +0300

    связал шаблоны с представлением и адресами

---
+++ 15_DatabasesAdvanced/marketplace/marketplace_cite/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('balance', views.BalancePage.as_view(), name='balance'),
+    path('login', views.AuthenticationView.as_view(), name='login'),
+    path('personal-cabinet', views.PersonalCabinetView.as_view(), name='personal-cabinet'),
+    path('popular-products', views.PopularProductsView.as_view(), name='popular-products'),
+    path('products-list', views.ProductsListView.as_view(), name='products-list'),
+    path('purchase', views.PurchaseView.as_view(), name='purchase'),
+    path('registration', views.RegistrationView.as_view(), name='registration'),
+    path('shopping-cart', views.ShoppingCartView.as_view(), name='shopping-cart'),
+]
--- 15_DatabasesAdvanced/marketplace/marketplace_cite/views.py
+++ 15_DatabasesAdvanced/marketplace/marketplace_cite/views.py
@@ -9,35 +9,42 @@ class BalancePage(View):
     def get(self, request):
         return render(request, 'marketplace_cite/add_balance_page.html', context={})
 
+
 class AuthenticationView(View):
 
     def get(self, request):
         return render(request, 'marketplace_cite/authentication_page.html', context={})
 
-class ProductsListView(View):
+
+class PersonalCabinetView(View):
 
     def get(self, request):
         return render(request, 'marketplace_cite/personal_cabinet_page.html', context={})
 
-class ProductsListView(View):
+
+class PopularProductsView(View):
 
     def get(self, request):
         return render(request, 'marketplace_cite/popular_products_page.html', context={})
+
 
 class ProductsListView(View):
 
     def get(self, request):
         return render(request, 'marketplace_cite/products_list_page.html', context={})
 
-class ProductsListView(View):
+
+class PurchaseView(View):
 
     def get(self, request):
         return render(request, 'marketplace_cite/purchase_page.html', context={})
+
 
 class RegistrationView(View):
 
     def get(self, request):
         return render(request, 'marketplace_cite/registration_page.html', context={})
+
 
 class ShoppingCartView(View):
 
